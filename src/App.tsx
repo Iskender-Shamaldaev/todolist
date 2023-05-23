058811070f296fd1863b31795ce77f425257bf14
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-05-23 18:14:33 +0600

    Added component Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import AddTaskForm from "./AddTaskForm/AddTaskForm";
+import Task from "./Task/Task";
 
 const App = () => (
     <div className="App">
       <AddTaskForm/>
+      <Task/>
     </div>
 );
 
---
+++ src/Task/Task.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Task = () => {
+    return (
+        <div className="task-wrap">
+            <p className="text">Add new Task</p>
+            <button className="btn-remove" >Remove</button>
+        </div>
+    );
+};
+
+export default Task;

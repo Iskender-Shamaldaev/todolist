223b51d20da08fcef91175bb1aa165e81c1b6d7d
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-05-25 02:00:24 +0600

    Task done

--- src/Task/Task.tsx
+++ src/Task/Task.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-interface IPersonProps extends React.PropsWithChildren {
+interface IMessageProps extends React.PropsWithChildren {
     text: string;
+    onRemoveMessage: React.MouseEventHandler;
 }
 
-const Task: React.FC<IPersonProps> = props => {
+const Task: React.FC<IMessageProps> = props => {
     return (
         <div className="task-wrap">
             <p className="text">{props.text}</p>
-            <button className="btn-remove">Remove</button>
+            <button className="btn-remove" onClick={props.onRemoveMessage}></button>
         </div>
     );
 };
